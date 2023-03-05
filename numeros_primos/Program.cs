8850992332c4a8528cf83b53b77fc88e7530bcf3
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-05 17:41:05 -0300

    Add project files.

---
+++ numeros_primos/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static int NumeroPrimo(double numero)
+    {
+        int divisores = 0;
+        for (int i = 1; i <= int.Parse(numero.ToString()); i++)
+        {
+            if (numero % i == 0)
+            {
+                divisores++;
+            }
+        }
+        return divisores;
+    }
+    private static void Main(string[] args)
+    {
+        double numero;
+        int divisoresTotal;
+
+        Console.Write("Informe um numero: ");
+        numero = double.Parse(Console.ReadLine());
+
+        divisoresTotal = NumeroPrimo(numero);
+
+        if (divisoresTotal == 2)
+        {
+            Console.WriteLine($"{numero} é um numero primo!");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} não é um numero primo!");
+        }
+    }
+}
